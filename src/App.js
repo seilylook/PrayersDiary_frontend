b5914398ba6b39690aacdf9c171d967ab50b90f6
seilylook@naver.com <seilylook@naver.com>
2021-06-27 17:49:05 +0900

    prayerdiay-frontend-start

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    isLoading: true,
+    groups: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch("/login");
+    const body = await response.json();
+    this.setState({ groups: body, isLoading: false });
+  }
+
+  render() {
+    const { groups, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-intro">
+            <h2>JUG List</h2>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
