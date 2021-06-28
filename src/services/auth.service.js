1d590e39aca8011b838d4546b63bf20ac988d6e4
seilylook@naver.com <seilylook@naver.com>
2021-06-28 15:40:11 +0900

    front-editing

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth";
+const API_URL = "http://localhost:8080/api/auth/";
 
 const register = (username, email, password) => {
   return axios.post(API_URL + "signup", {
@@ -10,10 +10,10 @@ const register = (username, email, password) => {
   });
 };
 
-const Login = (email, password) => {
+const login = (username, password) => {
   return axios
     .post(API_URL + "signin", {
-      email,
+      username,
       password
     })
     .then(response => {
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8080/api/test";
+const API_URL = "http://localhost:8080/api/test/";
 
 const getPublicContent = () => {
   return axios.get(API_URL + "all");
@@ -9,6 +9,10 @@ const getPublicContent = () => {
 
 const getUserBoard = () => {
   return axios.get(API_URL + "user", { headers: authHeader() });
+};
+
+const getModeratorBoard = () => {
+  return axios.get(API_URL + "mod", { headers: authHeader() });
 };
 
 const getAdminBoard = () => {
